Fixed bug in sensors azimuth angle calculation

sister/utils/geometry.py
@@ -304,9 +304,9 @@ def sensor_view_angles(sat_enu,grd_enu):
     sensor_az = np.degrees(np.arctan(p[0]/p[1]))
 
     DX,DY,DZ= grd_enu - sat_enu[:,np.newaxis]
-    sensor_az[DY>0]= -sensor_az[DY>0]
-    sensor_az[DX>0]= 360+sensor_az[DX>0]
-
+    sensor_az[(DX>0) & (DY>0)]= -sensor_az[(DX>0) & (DY>0)]
+    sensor_az[(DX>0) & (DY<0)]= 360+sensor_az[(DX>0) & (DY<0)]
+    sensor_az[(DX<0) & (DY>0)]= 180-sensor_az[(DX<0) & (DY>0]
 
     return sensor_zn,sensor_az
 