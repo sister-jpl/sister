cc02290c26c99edaf833c6b412d704671df06df5
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-05-14 10:33:10 -0500

    Added prisma processing scripts

---
+++ scripts/prisma/prisma_kml_extent.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SISTER
+Space-based Imaging Spectroscopy and Thermal PathfindER
+Author: Adam Chlus
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import argparse
+from collections import OrderedDict
+import os
+import shutil
+import zipfile
+import h5py
+import fiona
+from fiona.crs import from_epsg
+
+def main():
+    '''
+    This function exports the extents of a PRISMA scene to a KML file.
+
+    l1_zip: Path to zipped L1 data
+    out_dir: Output directory
+    temp_dir: Temporary directory
+
+    '''
+    parser = argparse.ArgumentParser(description = "Export PRISMA scene extent to KML")
+    parser.add_argument('l1_zip',help="Input zipped L1 HDF path", type = str)
+    parser.add_argument('out_dir',help="Output directory", type = str)
+    parser.add_argument('temp_dir',help="Temporary data directory", type = str)
+
+    args = parser.parse_args()
+    l1_zip  = args.l1_zip
+    base_name = os.path.basename(l1_zip).replace('.zip','')
+    temp_dir =  "%s/temp_%s/" % (args.temp_dir,base_name)
+    out_dir = args.out_dir
+
+    print('Unzipping %s' % base_name)
+    with zipfile.ZipFile(l1_zip,'r') as zipped:
+        zipped.extractall(temp_dir)
+    l1  = '%s/%s.he5' % (temp_dir,base_name)
+
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+    if not os.path.isdir(temp_dir):
+        os.mkdir(temp_dir)
+
+    l_obj = h5py.File(l1,'r')
+    geo =  l_obj['HDFEOS']["SWATHS"]['PRS_L1_HCO']['Geolocation Fields']
+
+    # Get extents of image
+    lon1 = geo['Longitude_VNIR'][0,0]
+    lon2 = geo['Longitude_VNIR'][0,-1]
+    lon3 = geo['Longitude_VNIR'][-1,-1]
+    lon4 = geo['Longitude_VNIR'][-1,0]
+
+    lat1 = geo['Latitude_VNIR'][0,0]
+    lat2 = geo['Latitude_VNIR'][0,-1]
+    lat3 = geo['Latitude_VNIR'][-1,-1]
+    lat4 = geo['Latitude_VNIR'][-1,0]
+
+    extent = {'geometry' : {'coordinates': [[(lon1,lat1),
+                            (lon2,lat2),
+                            (lon3,lat3),
+                            (lon4,lat4),
+                            (lon1,lat1)]],
+                            'type': 'Polygon'},
+              'properties' : OrderedDict([])}
+
+    schema = {'geometry': 'Polygon',
+              'properties': OrderedDict([])}
+
+    temp_geojson = '%s/temp.geojson' % temp_dir
+    with fiona.open(temp_geojson,
+                    'w',
+                    driver="GeoJSON",
+                    crs=from_epsg(4326),
+                    schema=schema) as c:
+        c.write(extent)
+    print('Converting to KML')
+    os.system('ogr2ogr %s/%s %s >/dev/null 2>&1' % (out_dir,os.path.basename(l1_zip).replace('zip','kml'),temp_geojson))
+    print('Deleting temporary files')
+    shutil.rmtree(temp_dir)
+
+if __name__== "__main__":
+    main()
--- scripts/prisma/prisma_radiometric_adjustment_factor_calculate.py
+++ scripts/prisma/prisma_radiometric_adjustment_factor_calculate.py
@@ -46,7 +46,7 @@ import shutil
 
 home = os.path.expanduser('~')
 
-
+# Potentential reference scenes
 base_names = ['20200715103934_20200715103938_0001',
               '20210209104043_20210209104047_0001',
               '20200721104249_20200721104253_0001']
@@ -55,9 +55,9 @@ export_figures = True
 for base_name in base_names:
 
     iso_base = '%s/isotest/isofit/' % home
-    figure_dir = '%s/sister/figures/prisma/wavelength_shift/' % home
+    figure_dir = '%s/sister/figures/prisma/radiance_factors/' % home
     doy = dt.datetime.strptime(base_name[:8],'%Y%m%d').timetuple().tm_yday
-    base_file =  '%s/data/prisma/rad/PRISMA_%s_wave_shift/PRISMA_%s_' % (home, base_name,base_name)
+    base_file =  '%s/data/prisma/rad/PRISMA_%s_rad_factor/PRISMA_%s_' % (home, base_name,base_name)
     cal_dir = '%s/temp/%s' % (home,base_name)
 
     if os.path.isdir(cal_dir):
@@ -78,6 +78,7 @@ for base_name in base_names:
     location = ht.HyTools()
     location.read_file(base_file + 'loc_unprj', 'envi')
 
+    #Get center of image
     obs_mean  =  observables.get_chunk(0,-1,0,-1).mean(axis =(0,1))
     loc_mean  =  location.get_chunk(0,-1,0,-1).mean(axis =(0,1))
 
@@ -99,7 +100,6 @@ for base_name in base_names:
                        surface_file,
                        surface_config)
     surface_model(surface_config)
-
 
     # Create modtran template file
     engine = 'sRTMnet'
@@ -123,11 +123,12 @@ for base_name in base_names:
     full_config = Config({"forward_model":{"instrument":instrument_config,
                                            "surface":surface_config,
                                            "radiative_transfer": rtm_config}})
-
+    #Start up ray
     if ray.is_initialized():
         ray.shutdown()
     ray.init(num_cpus = 48)
 
+    # Run intial forward model
     fm = ForwardModel(full_config)
 
     inversion_settings = {"implementation": {"mode": "inversion",
@@ -176,33 +177,38 @@ for base_name in base_names:
                                            "radiative_transfer": rtm_config}})
 
     fm = ForwardModel(full_config)
-
     inverse_config = Config(inversion_settings)
     iv = Inversion(inverse_config, fm)
 
-    obs_mean  =  observables.get_chunk(2,990,
-                                      400,600).mean(axis =(0,1))
-    loc_mean  =  location.get_chunk(2,990,
-                                      400,600).mean(axis =(0,1))
-    rad_mean  = radiance.get_chunk(2,990,
-                                      400,600).mean(axis =(0,1))
+    # Use the middle of the scene for calibration, least impacted by residual
+    # wavelength shift
+    line  =500
 
+    # Get mean line data
+    obs_mean  =  observables.get_chunk(0,-1,
+                                      line,line+1).mean(axis =(0,1))
+    loc_mean  =  location.get_chunk(0,-1,
+                                      line,line+1).mean(axis =(0,1))
+    rad_mean  = radiance.get_chunk(0,-1,
+                                 line,line+1).mean(axis =(0,1))
+
+    geom = Geometry(obs = obs_mean,loc = loc_mean)
     state_trajectory = iv.invert(rad_mean, geom)
-
     state_est = state_trajectory[-1]
     rfl_est1 = state_est[:radiance.bands]
 
+
+    # Generate smooth reflectance spectrum
     rfl_smooth = np.copy(rfl_est1)
     rfl_smooth[:30]= savgol_filter(rfl_smooth[:30],7,3)
-    d_start,d_end = 30,63
-    rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
-    d_start,d_end = 63,94
-    rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
+    d_start,d_end = 30,94
+    rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],31,2)
     d_start,d_end = 112,140
-    rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,4)
+    rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],25,1)
     d_start,d_end = 161,223
     rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
 
+    #Foward model to at sensor radiance
     new_state = state_est.copy()
     new_state[:radiance.bands] =rfl_smooth
     smooth_rad = fm.calc_rdn(new_state,geom)
@@ -220,6 +226,7 @@ for base_name in base_names:
     ax.set_xlim(375,2500)
     for direction in ['left','right','top','bottom']:
         ax.spines[direction].set_linewidth(1.5)
+
     if export_figures:
         plt.savefig("%s/PRISMA_%s_initial_rfl.png" % (figure_dir,base_name),
                     bbox_inches = 'tight', dpi = 500)
@@ -238,188 +245,42 @@ for base_name in base_names:
         ax.spines[direction].set_linewidth(1.5)
     if export_figures:
         plt.savefig("%s/PRISMA_%s_smoothed_rfl.png" % (figure_dir,base_name),
+        plt.show()
+                    bbox_inches = 'tight', dpi = 500)
+        plt.close()
+
+    #Calculate radiance factors, set water absorption bands to 1
+    ratio = smooth_rad/rad_mean
+    ratio[radiance.bad_bands] = 1
+    rad_mean[radiance.bad_bands] = np.nan
+
+    fig = plt.figure(figsize = (10,4),facecolor='w')
+    ax1 = fig.add_subplot(211)
+    ax1.plot(radiance.wavelengths, rad_mean,label= 'ISOFIT', c = 'r')
+    ax1.plot(radiance.wavelengths, smooth_rad,label= 'ISOFIT smooth', c = 'k')
+    ax1.legend(frameon = False)
+    ax1.set_xlabel('Wavelength (nm)')
+    ax1.set_ylabel('Reflectance')
+    ax1.legend(bbox_to_anchor=(.9,-.17), frameon= False, ncol= 2, fontsize = 12)
+
+    ax2 = fig.add_subplot(212)
+    ax2.plot(radiance.wavelengths, ratio, c = 'gray')
+    ax2.legend(frameon = False)
+    ax2.set_xlabel('Wavelength (nm)')
+    ax2.set_ylabel('Correction factor')
+    ax2.legend(bbox_to_anchor=(.9,-.17), frameon= False, ncol= 2, fontsize = 12)
+
+    if export_figures:
+        plt.savefig("%s/PRISMA_%s_smoothed_atsensor_radiance_factors.png" % (figure_dir,base_name),
                     bbox_inches = 'tight', dpi = 500)
         plt.show()
         plt.close()
 
-    #Plot smoother at-sensor radiance
-    fig = plt.figure(figsize = (6,4))
-    ax = fig.add_subplot(111)
-    ax.plot(radiance.wavelengths,smooth_rad,c='g',lw=2)
-    ax.set_xlabel("Wavelength (nm)",fontsize = 15)
-    ax.set_ylabel("Radiance",fontsize = 15)
-    ax.set_xlim(375,2500)
-    for direction in ['left','right','top','bottom']:
-        ax.spines[direction].set_linewidth(1.5)
-    if export_figures:
-        plt.savefig("%s/PRISMA_%s_smoothed_atsensor_radiance.png" % (figure_dir,base_name),
-                    bbox_inches = 'tight', dpi = 500)
-        plt.show()
-        plt.close()
 
-    shift_surf = np.zeros((radiance.lines,radiance.bands))
-
-    #Run every 10 lines and average
-    lines = [x for x in range(radiance.lines)]
-    for line in lines:
-        print(line)
-
-        # Get mean line data
-        obs_mean  =  observables.get_chunk(0,-1,
-                                          line,line+1).mean(axis =(0,1))
-        loc_mean  =  location.get_chunk(0,-1,
-                                          line,line+1).mean(axis =(0,1))
-        rad_mean  = radiance.get_chunk(0,-1,
-                                     line,line+1).mean(axis =(0,1))
-
-        # Perform inversion
-        geom = Geometry(obs = obs_mean,loc = loc_mean)
-        state_trajectory = iv.invert(rad_mean, geom)
-        state_est = state_trajectory[-1]
-        rfl_est1 = state_est[:radiance.bands]
-        rfl_est1[radiance.bad_bands] = np.nan
-
-        # Generate smoothed reflectance and forward model to at-sensor radiance
-        rfl_smooth = np.copy(rfl_est1)
-        rfl_smooth[:30]= savgol_filter(rfl_smooth[:30],7,3)
-        d_start,d_end = 30,63
-        rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
-        d_start,d_end = 63,94
-        rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
-        d_start,d_end = 112,140
-        rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,4)
-        d_start,d_end = 161,223
-        rfl_smooth[d_start:d_end] = savgol_filter(rfl_smooth[d_start:d_end],17,2)
-
-        new_state = state_est.copy()
-        new_state[:radiance.bands] =rfl_smooth
-        smooth_rad = fm.calc_rdn(new_state,geom)
-
-        # Fit line
-        new_rad = np.copy(rad_mean)
-        for d_start,d_end in [[0,63],[63,94],[112,140],[161,223]]:
-            mask = ~radiance.bad_bands[d_start:d_end]
-            waves= radiance.wavelengths[d_start:d_end][mask]
-            x = np.arange(d_end-d_start)[mask]
-            smooth_rad_sub = smooth_rad[d_start:d_end]
-
-            def poly_2(abc):
-                a,b,c =abc
-                shift= a + x*b + c*(x**2)
-                detect_interpolator = interp1d(waves+shift,rad_mean[d_start:d_end][mask],
-                                               fill_value = "extrapolate",kind='cubic')
-                rad_shift = detect_interpolator(waves)
-                rmse = np.sqrt(np.mean((rad_shift-smooth_rad_sub)**2))
-                return rmse
-
-            res = minimize(poly_2, (.0,.0,0), method='Nelder-Mead', tol=1e-6)
-            a,b,c= res.x
-            x_full = np.arange(d_end-d_start)
-            shift= a + x_full*b + c*(x_full**2)
-            shift_surf[line,d_start:d_end] = shift
-            detect_interpolator = interp1d(waves+shift,rad_mean[d_start:d_end][mask],
-                                           fill_value = "extrapolate",kind='cubic')
-            new_rad[d_start:d_end]= detect_interpolator(waves)
-
-        if line ==500:
-            shift_state = iv.invert(new_rad, geom)[-1,:-2]
-            shift_state[radiance.bad_bands] = np.nan
-            fig = plt.figure(figsize = (6,4))
-            ax = fig.add_subplot(111)
-            ax.plot(radiance.wavelengths,rfl_est1,alpha=1,c = 'k', label ='Unshifted')
-            ax.plot(radiance.wavelengths,shift_state,alpha=1,c = 'r',label = 'Shifted')
-            ax.legend(frameon = False,loc = 'lower right',fontsize= 12)
-            ax.set_xlabel("Wavelength (nm)",fontsize = 15)
-            ax.set_ylabel("Reflectance",fontsize = 15)
-            ax.set_xlim(375,2500)
-
-            for direction in ['left','right','top','bottom']:
-                ax.spines[direction].set_linewidth(1.5)
-
-            if export_figures:
-                plt.savefig("%s/PRISMA_%s_rfl_optimized.png" % (figure_dir,base_name),
-                                bbox_inches = 'tight', dpi = 500)
-                plt.show()
-                plt.close()
-
-    shift_header = envi_header_dict()
-    shift_header ['lines']= shift_surf.shape[0]
-    shift_header ['samples']= shift_surf.shape[1]
-    shift_header ['bands']= 1
-    shift_header ['interleave']= 'bsq'
-    shift_header ['data type'] = 4
-    shift_header['byte order'] = 0
-    shift_header['band_names'] = ['wavelength shift(nm)']
-    shift_file = "%s/sister/data/prisma/wavelength_shift/PRISMA_%s_wavelength_shift_surface" % (home,base_name)
-    writer = WriteENVI(shift_file,shift_header )
-    writer.write_band(shift_surf,0)
-
-    example_waves = [500,750,850,1200,1660,2200]
-    example_bands = [radiance.wave_to_band(wave) for wave in example_waves]
-
-    fig = plt.figure(figsize = (11,6))
-    a=1
-    shift_surf_smooth = np.zeros((radiance.lines,radiance.bands))
-    for band in range(radiance.bands):
-        wavelength= radiance.wavelengths[band]
-        shift = np.copy(shift_surf[lines,band])
-        z = np.polyfit(lines, shift, 3)
-        p = np.poly1d(z)
-        shift_surf_smooth[:,band] = p(np.arange(radiance.lines))
-
-        if band in example_bands:
-            ax = fig.add_subplot(2,3,a)
-            ax.scatter(lines,shift,c='gray')
-            ax.plot(shift_surf_smooth[:,band],c='r',lw=2)
-
-            if a >3:
-                ax.set_xlabel('Cross track pixel',fontsize = 15)
-            if a in [1,4]:
-                ax.set_ylabel('Shift (nm)',fontsize = 15)
-            ax.text(.02,.9,'%s nm' % int(wavelength),transform=ax.transAxes,fontsize = 13)
-
-            a+=1
-    if export_figures:
-        plt.savefig("%s/PRISMA_%s_shift_cross_track_fit.png" % (figure_dir,base_name),
-                        bbox_inches = 'tight', dpi = 500)
-        plt.show()
-        plt.close()
-
-
-    fig = plt.figure(figsize = (10,10))
-    ax = fig.add_subplot(111)
-    im = ax.matshow(shift_surf,
-                    cmap = plt.get_cmap('seismic'),
-                    vmin=-5,vmax=5)
-    ax.set_aspect(.3)
-    cb = plt.colorbar(im)
-    cb.set_label('Shift (nm)',fontsize =15)
-    ax.set_ylabel('Cross track pixel',fontsize = 15)
-    if export_figures:
-        plt.savefig("%s/PRISMA_%s_shift_cross_track_fit.png" % (figure_dir,base_name),
-                        bbox_inches = 'tight', dpi = 500)
-        plt.show()
-        plt.close()
-
-    shift_surf_smooth[:,radiance.bad_bands]=0
-    shift_header = envi_header_dict()
-    shift_header ['lines']= shift_surf_smooth.shape[0]
-    shift_header ['samples']= shift_surf_smooth.shape[1]
-    shift_header ['bands']= 1
-    shift_header ['interleave']= 'bsq'
-    shift_header ['data type'] = 4
-    shift_header['byte order'] = 0
-    shift_header['band_names'] = ['wavelength shift(nm)']
-
-    shift_file = "%s/sister/data/prisma/wavelength_shift/PRISMA_%s_wavelength_shift_surface_smooth" % (home,base_name)
-    writer = WriteENVI(shift_file,shift_header )
-    writer.write_band(shift_surf_smooth,0)
-
-
-
-
-
-
+    rad_corr_file = "%s/sister/data/prisma/radiance_factors/PRISMA_%s_radiance_factors.txt" % (home,base_name)
+    rad_corr = pd.DataFrame(index = [x for x in range(radiance.bands)])
+    rad_corr[0] = ratio
+    rad_corr.to_csv(rad_corr_file , sep = '\t',header=False)
 
 
 
