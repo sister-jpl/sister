2b99772ec26a9047b5d2755787251f47c0ea4139
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-05-14 10:34:23 -0500

    Added license info

--- sister/utils/ancillary.py
+++ sister/utils/ancillary.py
@@ -5,7 +5,19 @@ SISTER
 Space-based Imaging Spectroscopy and Thermal PathfindER
 Author: Adam Chlus
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 import logging
 from hytools.io.envi import WriteENVI,envi_header_dict
 
