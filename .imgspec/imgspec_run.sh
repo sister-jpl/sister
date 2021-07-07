20a25ffd19806ecd28c1e6bde27873451fea0928
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-07-07 16:20:25 -0400

    Updated input, output and temp directories

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# arg1: Output resolution in meters, should be a factor of 30 (ex. 30,60,90.....)
+# arg1: Output resolution in meters, should be a multiple of 30 (ex. 30,60,90.....)
 
 imgspec_dir=$( cd "$(dirname "$0")" ; pwd -P )
 sister_dir=$(dirname ${imgspec_dir})
 
 aws_cop_url='https://copernicus-dem-30m.s3.amazonaws.com/'
 shift_surface='https://github.com/EnSpec/sister/raw/master/data/prisma/wavelength_shift/PRISMA_20200721104249_20200721104253_0001_wavelength_shift_surface'
-output_dir='/data2/prisma/rdn/'
-temp_dir='/data2/temp/'
-l1_zip=${sister_dir}/input/PRS*.zip
+output_dir='output'
+temp_dir='tmp'
+l1_zip=input/PRS*.zip
+
+mkdir -p $output_dir
+mkdir -p $temp_dir
 
 # Run PRISMA PGE, export rdn, obs and loc ENVI files
-python ${sister_dir}/scripts/prisma/prisma_pge.py ${l1_zip} ${output_dir} ${temp_dir} ${aws_cop_url} -proj -res $1 -shift ${shift_surface}
+python ${sister_dir}/scripts/prisma/prisma_pge.py $l1_zip $output_dir $temp_dir $aws_cop_url -proj -res $1 -shift $shift_surface
