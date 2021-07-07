63e03a1f8e5e4aeff2431e8126a8ee14e715a204
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-07-07 14:25:44 -0400

    Removed positional arguments, all args are hard coded or infered

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# arg1: PRISMA L1 zip file
-# arg2: Output directory
-# arg3: Temporary directory
 
 imgspec_dir=$( cd "$(dirname "$0")" ; pwd -P )
 sister_dir=$(dirname ${imgspec_dir})
 
 aws_cop_url='https://copernicus-dem-30m.s3.amazonaws.com/'
 shift_surface='https://github.com/EnSpec/sister/raw/master/data/prisma/wavelength_shift/PRISMA_20200721104249_20200721104253_0001_wavelength_shift_surface'
+output_dir='/data2/prisma/rdn/'
+temp_dir='/data2/temp/'
+l1_zip=${sister_dir}/input/PRS*.zip
 
-python ${sister_dir}/scripts/prisma/prisma_pge.py $1 $2 $3 ${aws_cop_url} -proj -res 30 -shift ${shift_surface}
+# Run PRISMA PGE, export rdn, obs and loc ENVI files
+python ${sister_dir}/scripts/prisma/prisma_pge.py ${l1_zip} ${output_dir} ${temp_dir} ${aws_cop_url} -proj -res 30 -shift ${shift_surface}
