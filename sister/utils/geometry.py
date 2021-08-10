88782793e14f3449e1fe77ec756628bb548089c5
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-08-10 08:57:46 -0400

    Changed GEE Landsat8 dataset

--- sister/utils/geometry.py
+++ sister/utils/geometry.py
@@ -352,7 +352,6 @@ def get_landsat_image(longitude,latitude,month,max_cloud = 5,band=5,project = Tr
 
     #Retrieve Landsat 8 collection and average
     landsat8 = ee.ImageCollection("LANDSAT/LC08/C01/T1")
-
     landsat8_bounds = landsat8.filterBounds(bounds)
     landsat8_month = landsat8_bounds.filter(ee.Filter.calendarRange(month-1,month+1,'month'))
     landsat8_cloud = landsat8_month.filterMetadata('CLOUD_COVER','less_than',max_cloud).sort('CLOUDY_PIXEL_PERCENTAGE')
