70628f059eddfe267b7d59c2f76a254d7e83e817
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-26 16:59:56 -0400

    Run script update

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-# arg1: Output resolution in meters, should be a multiple of 30 (ex. 30,60,90.....)
 
-imgspec_dir=$( cd "$(dirname "$0")" ; pwd -P )
-sister_dir=$(dirname ${imgspec_dir})
+imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
+pge_dir=$(dirname ${imgspec_dir})
 
-aws_cop_url='https://copernicus-dem-30m.s3.amazonaws.com/'
-shift_surface='https://github.com/EnSpec/sister/raw/master/data/prisma/wavelength_shift/PRISMA_20200721104249_20200721104253_0001_wavelength_shift_surface'
-output_dir='output'
-temp_dir='tmp'
-l1_zip=input/PRS*.zip
+conda create -y --name sister python=3.8
+source activate sister
+conda install -yc conda-forge gdal
+conda install -y numba
 
-mkdir -p $output_dir
-mkdir -p $temp_dir
+cd $pge_dir
+python setup.py install
+yes | pip uninstall ray
+yes | pip install 'ray[default]'
 
-# Run PRISMA PGE, export rdn, obs and loc ENVI files
-python ${sister_dir}/scripts/prisma/prisma_pge.py $l1_zip $output_dir $temp_dir $aws_cop_url -proj -res $1 -shift $shift_surface
+cd $imgspec_dir
 
-# gzip output files in preparation for downstream processing
-cd $output_dir
-l1_output_dir=$(ls -d PRS*)
-tar -cf ${l1_output_dir}.tar ${l1_output_dir}
-rm -rf $l1_output_dir
-gzip ${l1_output_dir}.tar
+mkdir output
+mkdir temp
+
+input_file=$(ls input/*.*)
+
+python ${pge_dir}/scripts/preprocess.py $input_file output/ temp/
+
+cd output
+out_dir= $(ls ./)
+tar -czvf $out_dir.tar.gz $out_dir
+rm -r $out_dir
