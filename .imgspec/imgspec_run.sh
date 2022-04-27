1316dd422ab27d24e955b764fa10a9c624fcbe7c
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-27 17:59:59 -0400

    Added suffix to tar.gz output

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -23,5 +23,5 @@ python ${pge_dir}/scripts/l1_preprocess.py $input_file output/ temp/
 
 cd output
 out_dir=$(ls ./)
-tar -czvf $out_dir.tar.gz $out_dir
+tar -czvf $out_dir_l1p.tar.gz $out_dir
 rm -r $out_dir
