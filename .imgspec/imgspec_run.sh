e5241c31c9d825fe9cc47307e1dd53ec769bb861
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-27 13:58:08 -0400

    Added gdal version

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -4,9 +4,10 @@ run_dir=$('pwd')
 imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
+# Need to do custom install to prevent dependency errors
 conda create -y --name sister python=3.8
 source activate sister
-conda install -yc conda-forge gdal
+conda install -yc conda-forge gdal=3.4.1
 conda install -y numba
 
 cd $pge_dir
