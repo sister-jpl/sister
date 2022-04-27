b40b724608c71feba2f29ff4347d0931a5866cc1
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-27 08:21:33 -0400

    Fix bug

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+run_dir=$('pwd')
 imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
-run_dir=$('pwd')
+
 conda create -y --name sister python=3.8
 source activate sister
 conda install -yc conda-forge gdal
