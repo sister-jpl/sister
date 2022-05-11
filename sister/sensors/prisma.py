Fixed bug in geometry averaging

sister/sensors/prisma.py
@@ -345,8 +345,8 @@ def he5_to_envi(l1_zip,out_dir,temp_dir,elev_dir,shift = False, rad_coeff = Fals
         project.query_tree(warp_east,warp_north,pixel_size)
 
         # Project independent variables
-        sensor_az_prj = project.project_band(sensor_az,-9999)
-        sensor_zn_prj = project.project_band(sensor_zn,-9999)
+        sensor_az_prj = project.project_band(sensor_az,-9999,angular=True)
+        sensor_zn_prj = project.project_band(sensor_zn,-9999,angular=True)
         elevation_prj = project.project_band(elevation.astype(np.float),-9999)
 
         radiance = ht.HyTools()