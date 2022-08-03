c161c01ec7cacf15691535f7f60d4a9c1c090ff2
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-08-03 16:45:11 -0400

    Fixed installer

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -6,8 +6,6 @@ pge_dir=$(dirname ${imgspec_dir})
 conda create -y --name sister python=3.8
 source activate sister
 conda install -y gdal
-conda install -y numba
-pip install ray==1.9
 
 cd $pge_dir
-python setup.py install
+pip install .
