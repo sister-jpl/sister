080d136d97c057189abd7b4c24dd268f6e5b43d1
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-08-05 11:34:01 -0400

    Debugging code added

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -16,12 +16,20 @@ if [[ $base == PRS* ]]; then
     landsat_url=s3://sister-ops-workspace/prisma/landsat_reference/PRS_${timestamp}_landsat.tar.gz
     echo $landsat_url
     aws s3 cp $landsat_url ./input
-
     lst_archive=$(ls input/*landsat.tar.gz)
     tar -xzvf $lst_archive -C input/
     landsat=$(ls input/*landsat)
     rdn_coeffs=${pge_dir}/data/prisma/*_radcoeff_surface.npz
     smile=${pge_dir}/data/prisma/*_wavelength_shift_surface_smooth.npz
+
+    echo "INPUT"
+    echo $input_file
+    echo "###################################"
+
+    echo "COMMAND"
+    echo ${pge_dir}/scripts/l1_preprocess.py $input_file output/ temp/ $1 $smile $rdn_coeffs $landsat
+    echo "###################################"
+
     python ${pge_dir}/scripts/l1_preprocess.py $input_file output/ temp/ $1 $smile $rdn_coeffs $landsat
 else
     python ${pge_dir}/scripts/l1_preprocess.py $input_file output/ temp/ $1
