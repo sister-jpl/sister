Fixed aspect computation bug

sister/sensors/desis.py
@@ -242,7 +242,7 @@ def l1b_process(l1b_zip,out_dir,temp_dir,elev_dir,
     latitude = griddata(points, lat_vals, (lines,columns), method='linear')[:,85:]
 
     #Create initial elevation raster
-    elevation= dem_generate(longitude,latitude,elev_dir,temp_dir)
+    elevation,slope,aspect= terrain_generate(longitude,latitude,elev_dir,temp_dir)
     zone,direction = utm_zone(longitude,latitude)
 
     solar_az = solar.get_azimuth(latitude,longitude,start_time)
@@ -346,12 +346,11 @@ def l1b_process(l1b_zip,out_dir,temp_dir,elev_dir,
 
         #Recalculate elevation with new coordinates
         logging.info('Rebuilding DEM')
-        elevation= dem_generate(longitude,latitude,elev_dir,temp_dir)
+        elevation,slope,aspect= terrain_generate(longitude,latitude,elev_dir,temp_dir)
 
     loc_export(loc_file,longitude,latitude,elevation)
 
     # Generate remaining observable layers
-    slope,aspect = slope_aspect(elevation,temp_dir)
     cosine_i = calc_cosine_i(np.radians(solar_zn),
                              np.radians(solar_az),
                              np.radians(slope),
@@ -590,7 +589,7 @@ def l1c_process(l1c_zip,out_dir,temp_dir,elev_dir):
     solar_zn[mask] = -9999
 
     #Create elevation raster
-    elevation= dem_generate(longitude,latitude,elev_dir,temp_dir)
+    elevation,slope,aspect= terrain_generate(longitude,latitude,elev_dir,temp_dir)
     elevation[mask] = -9999
     longitude[mask] = -9999
     latitude[mask] = -9999
@@ -629,7 +628,6 @@ def l1c_process(l1c_zip,out_dir,temp_dir,elev_dir):
     sensor_zn[mask] = -9999
 
     # Generate remaining observable layers
-    slope,aspect = slope_aspect(elevation,temp_dir)
     cosine_i = calc_cosine_i(np.radians(solar_zn),
                              np.radians(solar_az),
                              np.radians(slope),

sister/sensors/prisma.py
@@ -273,8 +273,8 @@ def he5_to_envi(l1_zip,out_dir,temp_dir,elev_dir,shift = False, rad_coeff = Fals
     longitude= geo['Longitude_VNIR'][2:-2,2:-2]
     latitude= geo['Latitude_VNIR'][2:-2,2:-2]
 
-    #Create initial elevation raster
-    elevation= dem_generate(longitude,latitude,elev_dir,temp_dir)
+    #Create initial terrain datsets
+    elevation,slope,aspect= terrain_generate(longitude,latitude,elev_dir,temp_dir)
     zone,direction = utm_zone(longitude,latitude)
 
     # Calculate satellite X,Y,Z position for each line
@@ -421,14 +421,13 @@ def he5_to_envi(l1_zip,out_dir,temp_dir,elev_dir,shift = False, rad_coeff = Fals
 
         #Recalculate elevation with new coordinates
         logging.info('Rebuilding DEM')
-        elevation= dem_generate(longitude,latitude,elev_dir,temp_dir)
+        elevation,slope,aspect= terrain_generate(longitude,latitude,elev_dir,temp_dir)
 
 
     # Export location datacube
     loc_export(loc_file,longitude,latitude,elevation)
 
-    # Generate remaining observable layers
-    slope,aspect = slope_aspect(elevation,temp_dir)
+
     cosine_i = calc_cosine_i(np.radians(solar_zn),
                              np.radians(solar_az),
                              np.radians(slope),

sister/utils/terrain.py
@@ -35,7 +35,7 @@ from scipy.spatial import cKDTree
 from .misc import download_file
 
 
-def dem_generate(longitude,latitude,elev_dir,temp_dir):
+def terrain_generate(longitude,latitude,elev_dir,temp_dir):
     '''
     Args:
         longitude (float): Longitude array
@@ -75,97 +75,77 @@ def dem_generate(longitude,latitude,elev_dir,temp_dir):
     tiles_intersect = [tiles[n] for n in idx.intersection((lon_min, lat_min, lon_max, lat_max))]
 
     if len(tiles_intersect) == 0:
-        constant_elev = float(input("No overlapping tiles found, enter constant elevation for scene (m): "))
-        elevation = np.ones(longitude.shape) * constant_elev
-    else:
-        tile_string = "Found %s intersecting elevation tiles:" % len(tiles_intersect)
-        for tile in tiles_intersect:
-            tile_string+= '\n\t%s' % tile
+        raise ValueError('No overlapping Copernicus DEM tiles found.')
 
-            if 'aws' in elev_dir:
-                tile_url = "%s%s/%s.tif" % (elev_dir,tile,tile)
-                tile_file = "%s%s.tif" % (temp_dir,tile)
-                download_file(tile_file,tile_url)
-            else:
-                with tarfile.open(tile, 'r') as tar_ref:
-                    tar_ref.extractall(temp_dir)
-        logging.info(tile_string)
+    tile_string = "Found %s intersecting elevation tiles:" % len(tiles_intersect)
+    for tile in tiles_intersect:
+        tile_string+= '\n\t%s' % tile
+
+        if 'aws' in elev_dir:
+            tile_url = "%s%s/%s.tif" % (elev_dir,tile,tile)
+            tile_file = "%s%s.tif" % (temp_dir,tile)
+            download_file(tile_file,tile_url)
+        else:
+            with tarfile.open(tile, 'r') as tar_ref:
+                tar_ref.extractall(temp_dir)
+    logging.info(tile_string)
 
     logging.info('Merging DEM tiles')
     dem_file  = '%stemp_dem' % temp_dir
     os.system('gdal_merge.py -o %s -of ENVI %sCopernicus_DSM*' % (dem_file,temp_dir))
 
-    dem_obj = ht.HyTools()
-    dem_obj.read_file(dem_file, 'envi')
-
-    ulx = float(dem_obj.map_info[3])
-    uly = float(dem_obj.map_info[4])
-    pix_x = float(dem_obj.map_info[5])
-    pix_y = float(dem_obj.map_info[6])
-
-    dem_lat,dem_lon = np.indices((dem_obj.lines,dem_obj.columns))
-
-    dem_xl = int((lon_min-ulx)//pix_x)
-    dem_xr = int((lon_max-ulx)//pix_x)
-    dem_yu = int((uly-lat_max)//pix_y)
-    dem_yd = int((uly-lat_min)//pix_y)
-
-    dem_subset = dem_obj.get_chunk(dem_xl,dem_xr,dem_yu,dem_yd)
-    dem_lat,dem_lon = np.indices(dem_subset.shape[:2])
-    dem_lat = (lat_max- dem_lat*pix_y).flatten()
-    dem_lon = (lon_min+ dem_lon*pix_x).flatten()
-
-    #Create spatial index and nearest neighbor sample
-    src_points =np.concatenate([np.expand_dims(dem_lon,axis=1),
-                                np.expand_dims(dem_lat,axis=1)],axis=1)
-    tree = cKDTree(src_points,balanced_tree= False)
-
-    dst_points = np.concatenate([longitude.flatten()[:,np.newaxis],
-                                 latitude.flatten()[:,np.newaxis]],
-                                 axis=1)
-
-    indexes = tree.query(dst_points,k=1)[1]
-    indices_int = np.unravel_index(indexes,(dem_subset.shape[0],
-                                            dem_subset.shape[1]))
-    elevation = dem_subset[indices_int[0],indices_int[1]].reshape(longitude.shape)
-
-    #Set negative elevations to 0
-    if np.sum(elevation<0) > 0:
-        logging.warning('Elevations below sea level found, setting to 0m')
-        elevation[elevation<0] =0
-
-    return elevation
-
-def slope_aspect(elevation,temp_dir):
-    ''' Use GDAL to calculte slope and aspect
-    '''
-
-    dem_dict  = envi_header_dict()
-    dem_dict ['lines']= elevation.shape[0]
-    dem_dict ['samples']= elevation.shape[1]
-    dem_dict ['bands']= 1
-    dem_dict ['interleave']= 'bsq'
-    dem_dict ['data type'] = 4
-
-    dem_file = '%stemp_dem_clip' % temp_dir
-    writer = WriteENVI(dem_file,dem_dict)
-    writer.write_band(elevation,0)
-
-    slope_file =  '%s_slope' % temp_dir
-    aspect_file =  '%s_aspect' % temp_dir
+    slope_file =  '%stemp_slope' % temp_dir
+    aspect_file =  '%stemp_aspect' % temp_dir
 
     logging.info('Calculating slope')
-    os.system('gdaldem slope -of ENVI %s %s'% (dem_file,slope_file))
+    os.system('gdaldem slope -compute_edges -of ENVI %s %s'% (dem_file,slope_file))
 
     logging.info('Calculating aspect')
-    os.system('gdaldem aspect -f ENVI %s %s' % (dem_file,aspect_file))
+    os.system('gdaldem aspect -compute_edges -of ENVI %s %s' % (dem_file,aspect_file))
 
-    asp_obj = ht.HyTools()
-    asp_obj.read_file(aspect_file, 'envi')
-    aspect =asp_obj.get_band(0)
+    terrain_arrs = []
 
-    slp_obj = ht.HyTools()
-    slp_obj.read_file(slope_file, 'envi')
-    slope =slp_obj.get_band(0)
+    for file in [dem_file,slope_file,aspect_file]:
 
-    return slope,aspect
+        trr_obj = ht.HyTools()
+        trr_obj.read_file(file, 'envi')
+
+        ulx = float(trr_obj.map_info[3])
+        uly = float(trr_obj.map_info[4])
+        pix_x = float(trr_obj.map_info[5])
+        pix_y = float(trr_obj.map_info[6])
+
+        trr_lat,trr_lon = np.indices((trr_obj.lines,trr_obj.columns))
+
+        trr_xl = int((lon_min-ulx)//pix_x)
+        trr_xr = int((lon_max-ulx)//pix_x)
+        trr_yu = int((uly-lat_max)//pix_y)
+        trr_yd = int((uly-lat_min)//pix_y)
+
+        trr_subset = trr_obj.get_chunk(trr_xl,trr_xr,trr_yu,trr_yd)
+        trr_lat,trr_lon = np.indices(trr_subset.shape[:2])
+        trr_lat = (lat_max- trr_lat*pix_y).flatten()
+        trr_lon = (lon_min+ trr_lon*pix_x).flatten()
+
+        #Create spatial index and nearest neighbor sample
+        src_points =np.concatenate([np.expand_dims(trr_lon,axis=1),
+                                    np.expand_dims(trr_lat,axis=1)],axis=1)
+        tree = cKDTree(src_points,balanced_tree= False)
+
+        dst_points = np.concatenate([longitude.flatten()[:,np.newaxis],
+                                     latitude.flatten()[:,np.newaxis]],
+                                     axis=1)
+
+        indexes = tree.query(dst_points,k=1)[1]
+        indices_int = np.unravel_index(indexes,(trr_subset.shape[0],
+                                                trr_subset.shape[1]))
+        terrain = trr_subset[indices_int[0],indices_int[1]].reshape(longitude.shape)
+
+        #Set negative elevations to 0
+        if (np.sum(terrain<0) > 0) and 'dem' in file:
+            logging.warning('Elevations below sea level found, setting to 0m')
+            terrain[terrain<0] =0
+
+        terrain_arrs.append(terrain)
+
+    return terrain_arrs