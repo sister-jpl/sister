d3ea1de6e1481296b6bd36009b699350db140715
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-04-26 15:56:03 -0700

    Specified number of CPUs

--- sister/utils/geometry.py
+++ sister/utils/geometry.py
@@ -233,7 +233,7 @@ def image_match(ref_band,warp_band,offset_x,offset_y,sensor_zn_prj,sensor_az_prj
 
     if ray.is_initialized():
         ray.shutdown()
-    ray.init(num_cpus=13)
+    ray.init(num_cpus=10)
     logging.info('Ray intitialized')
 
     # Share arrays
